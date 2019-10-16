b678d5df04eead20dd1f6b70e56b3866526b3ee6
Vincent Guegan <gueganvincent@gmail.com>
2019-10-16 11:05:59 +0200

    changed file structure

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/model.php';
-
-
-$title = "hello world";
-$array = getFooBarBaz();
-
-require __DIR__.'/view.php';
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__.'/../models/model.php';
+
+
+$title = "hello world";
+$array = getFooBarBaz();
+
+require __DIR__.'/../views/view.php';
