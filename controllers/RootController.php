a872cf20b4924f420d399a004a2643e38558e30d
Vincent Guegan <gueganvincent@gmail.com>
2019-10-16 15:17:53 +0200

    typed return value in RootController.php

--- controllers/RootController.php
+++ controllers/RootController.php
@@ -6,7 +6,7 @@ require __DIR__.'/../models/FooBarBaz.php';
 
 class RootController extends Controller {
     
-    public function get() {
+    public function get(): string {
         return view('root', [
             'title' => "hello world",
             'array' => FooBarBaz::get(),
